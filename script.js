a582241994b0384119bfbd977a6f998968598161
Josh Rehanek <joshrehanek@gmail.com>
2020-11-29 14:52:15 -0800

    q&a array, newQuestion(), validateAnswer()

--- script.js
+++ script.js
@@ -1,112 +1,102 @@
 // DOM manipulation elements 
-const question = document.getElementById("question");
-const answers = Array.from(document.getElementsByClassName('answer-text'));
+const questionTitle = document.getElementById("question");
 const timeEl = document.querySelector(".time")
+const answerZoneEl = document.getElementById("answer-zone");
+const rightWrongEl = document.querySelector("#right-wrong");
 
 // Initial variables
 let currentQuestion = {};
-let acceptAnswers = false; 
+let acceptAnswers = false;
 let unusedQuestions = [];
 const correctPoints = 10;
 const wrongPoints = -15;
+let questionIndex = 0;
+let secondsLeft = 75;
 
 // Questions & Answers Object
 let questions = [
     {
-        question: "WHat is the HTML tag used to write Javascript?",
-        answer1: "<js>",
-        answer2: "<script>",
-        answer3: "<java>",
-        answer4: "<javascript>",
-        answer: 2
+        question: "What is the HTML tag used to write Javascript?",
+        choices: ["<js>", "<script>", "<java>", "<javascript>"],
+        answer: "<script>"
     },
     {
         question: "Which of the following is the correct syntax to display 'Yip Yip' in an alert box using JavaScript?",
-        answer1: "msg(“Yip Yip”);",
-        answer2: "alertbox(“Yip Yip”);",
-        answer3: "alert(“Yip Yip”);",
-        answer4: "prompt(“Yip Yip”);",
-        answer: 3
-    }, 
-    {
-        question: "What is the correct syntax for referring to an external script called 'avatar.js'?",
-        answer1: "<script name=”avatar.js”>",
-        answer2: "<script href=”avatar.js”>",
-        answer3: "<script ref=”avatar.js”>",
-        answer4: "<script src=”avatar.js”>",
-        answer: 4
+        choices: ["msg('Yip Yip'); ", "alertbox('Yip Yip'); ", "alert('Yip Yip');", "prompt('Yip Yip');"],
+        answer: "alert('Yip Yip');"
     },
     {
-        question: "Choose the correct JavaScript syntax to change the content of some HTML code with where the id ='avatar'",
+        question: "What company invented Javascript?",
+        choices: ["Netscape", "Facebook", "Mozilla", "Google"],
+        answer: "Netscape"
+    },
+    {
+        question: "Using best practice, how do you create a function in JavaScript?",
+        choices: ["function myFunction()", "function = myFunction()", "function myfunction()", "function:myFunction()"],
+        answer: "function myFunction()"
+    },
+    {
+        question: "How to write an IF statement in JavaScript?",
+        choices: ["if (i == 5)", "if i == 5", "if i = 5", "if i == 5 then"],
+        answer: "if (i == 5)"
+    },
 
-        answer1: "document.getElement(“avatar”).innerHTML=”I am the last airbender”;",
-        answer2: " document.getElementById(“avatar”).innerHTML=”I am a the last airbender”;",
-        answer3: "document.getId(“avatar”)=”I am a the last airbender”;",
-        answer4: "document.getElementById(“avatar”).innerHTML=I am a the last airbender;",
-        answer: 2
-    },
-    {
-        question: "Which built-in method combines the text of two strings and returns a new string?",
-        answer1: "concat()",
-        answer2: "append()",
-        answer3: "attach()",
-        answer4: "toLowerCase",
-        answer: 1
-    },
 ];
-
 // start game function using fat arrow syntax
-startGame = () => {
-    let secondsLeft = 5;
-    
+const startGame = () => {
 
-        var timerInterval = setInterval(function() {
-          secondsLeft--;
-          timeEl.textContent = secondsLeft + " seconds left";
-      
-          if(secondsLeft === 0) {
-            return window.location.assign("./end-game.html");    
-          }
-      
-        }, 1000);
-// uses spread operator to get a full copy questions from the array
-    unusedQuestions = [...questions];
-    console.log(unusedQuestions);
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timeEl.textContent = `You have ${secondsLeft} seconds left`;
+
+        if (secondsLeft === 0) {
+            return window.location.assign("./end-game.html");
+        }
+
+    }, 1000);
+
+    // console.log(unusedQuestions);
     newQuestion();
 };
 // New question function
-newQuestion = () => {
+const newQuestion = () => {
 
-    if (unusedQuestions.length === 0) {
+    answerZoneEl.innerHTML = '';
+    rightWrongEl.innerHTML = '';
+
+    let currentQuestion = questions[questionIndex];
+
+    if (questionIndex === questions.length) {
         return window.location.assign("./end-game.html");
     }
-    
 
-    const questionIndex = Math.floor(Math.random() * unusedQuestions.length);
-    console.log(questionIndex);
-    currentQuestion = unusedQuestions[questionIndex];
-    question.innerText = currentQuestion.question; 
+    questionTitle.textContent = currentQuestion.question;
 
-    answers.forEach((answer) => {
-        const number = answer.dataset['number'];
-        answer.innerText = currentQuestion["answer" + number];
-        console.log(number);
+    currentQuestion.choices.forEach((answer) => {
+        let tempBtn = document.createElement("button");
+        tempBtn.textContent = answer;
+        tempBtn.setAttribute('value', answer);
+        tempBtn.setAttribute('class', 'button-choice');
+        tempBtn.onclick = validateAnswer;
+        answerZoneEl.appendChild(tempBtn);
     });
-
-    unusedQuestions.splice(questionIndex, 1);
-    acceptAnswers = true;
 };
 
-answers.forEach((answer) => {
-    answer.addEventListener("click", (e) => {
-        if(!acceptAnswers) return;
+function validateAnswer() {
+    console.log(`value: ${this.value}`)
+    console.log(`answer: ${questions[questionIndex].answer}`)
+    if (this.value !== questions[questionIndex].answer) {
+        secondsLeft -= 15;
+        rightWrongEl.textContent = "WRONG";
+    } else {
+        rightWrongEl.textContent = "RIGHT";
+    }
 
-        acceptAnswers = false;
-        const choiceSelected = e.target;
-        const answerSelected = choiceSelected.dataset["number"];
-        console.log(answerSelected);
+    questionIndex++;
+    setTimeout(function () {
         newQuestion();
-    });
-});
+    }, 1000)
+
+}
 
 startGame();
