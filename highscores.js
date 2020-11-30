6706ce0a1bd9b70f5be47603c4299154412ad726
Josh Rehanek <joshrehanek@gmail.com>
2020-11-30 14:45:55 -0800

    adds vars and equation to return highscores list

---
+++ highscores.js
@@ -0,0 +1,17 @@
+// variables
+const highScoresListEl = document.getElementById('highScoresList');
+const highScoresEl = JSON.parse(localStorage.getItem('highScores')) || [];
+
+console.log(highScoresEl)
+
+// Map object holds key-value pairs and remembers the original insertion order of the keys.
+// returns list of high scores in the HTML's innertext
+highScoresListEl.innerHTML =
+    highScoresEl.map(score => {
+        return `<li class='high-score'>${score.name}-${score.score}</li>`;
+    }) 
+    // joins list info
+        .join('');
+
+
+
