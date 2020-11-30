82c146929b675a17e55621c220e163634a43e90e
Josh Rehanek <joshrehanek@gmail.com>
2020-11-29 18:15:32 -0800

    adds vars and calls localStorage to pull score

--- end-game.js
+++ end-game.js
@@ -1,3 +1,15 @@
+const usernameEl = document.getElementById('username');
+const saveScoreBtnEl = document.getElementById('saveScoreBtn');
+const finalScoreEl = document.getElementById('finalScore');
+const mostRecentScoreEl = localStorage.getItem('mostRecentScore');
+finalScoreEl.innerText = mostRecentScoreEl;
+
+usernameEl.addEventListener('keyup', () =>{
+    saveScoreBtnEl.disabled = !usernameEl.value;
+    console.log(usernameEl.value)
+});
+
 saveHighScore = e => {
-    
+    console.log('clicked')
+    e.preventDefault();
 }
