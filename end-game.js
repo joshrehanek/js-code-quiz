98d9127c3a13e598332619a2edbe94719944b263
Josh Rehanek <joshrehanek@gmail.com>
2020-11-30 13:38:49 -0800

    adds functions to save high scores to local stor

--- end-game.js
+++ end-game.js
@@ -2,7 +2,10 @@ const usernameEl = document.getElementById('username');
 const saveScoreBtnEl = document.getElementById('saveScoreBtn');
 const finalScoreEl = document.getElementById('finalScore');
 const mostRecentScoreEl = localStorage.getItem('mostRecentScore');
-finalScoreEl.innerText = mostRecentScoreEl;
+finalScoreEl.innerText = `You scored ${mostRecentScoreEl} points`;
+
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+console.log(highScores);
 
 usernameEl.addEventListener('keyup', () =>{
     saveScoreBtnEl.disabled = !usernameEl.value;
@@ -12,4 +15,17 @@ usernameEl.addEventListener('keyup', () =>{
 saveHighScore = e => {
     console.log('clicked')
     e.preventDefault();
-}
+
+    const score = {
+        score:mostRecentScoreEl,
+        name: usernameEl.value
+    };
+    
+    highScores.push(score);
+
+    highScores.sort( (a,b) => b.score - a.score);
+
+    highScores.splice(10);
+    
+    console.log(score);
+};
